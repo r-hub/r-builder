91f19a0c8bb2040c56d05c2baea9a3cf24ceda2f
Gábor Csárdi <csardi.gabor@gmail.com>
2020-10-29 08:09:16 +0000

    Make svn.sh work if the cert is ok

--- software/svn.sh
+++ software/svn.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/expect -f
 
 spawn svn list https://svn.r-project.org
-expect "(R)eject, accept (t)emporarily or accept (p)ermanently? "
-send -- "p\r"
+expect {
+    timeout                                                    { send_user "\nsvn time out\n"; exit 1 }
+    "(R)eject, accept (t)emporarily or accept (p)ermanently? " { send -- "p\r" }
+    eof
+}
